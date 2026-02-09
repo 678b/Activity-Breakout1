6b3f87f6fa6e5c08a9474f5368e1271593abeac1
Brian HIRWA NTWALI <hirwa371@gmail.com>
2026-02-08 23:57:27 -0800

    Updated script part 6

--- the_creator.sh
+++ the_creator.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-# This script creates students_directory with 3 cohort files using loops
 
-# Check if directory exists
-if [ -d "students_directory" ]
-then
-  echo "Directory already exists"
-else
-  mkdir students_directory
-  echo "Directory created"
-fi
+students_dir="students_directory"
 
-# Use loop to create cohort files
-for i in {1..3}
+for i in 1 2 3
 do
-  touch students_directory/cohort-$i.txt
-  echo "Created cohort-$i.txt"
+  echo -e "StudentA\nStudentB\nStudentC" > $students_dir/cohort-$i.txt
+  echo "Contents of cohort-$i.txt:"
+  cat $students_dir/cohort-$i.txt
+  echo "---------------------"
 done
 
-echo "This is the updated script!"
-EOF
