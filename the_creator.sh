c5b656f78d4b24bfcd26f8289020ae8643e6b152
Brian HIRWA NTWALI <hirwa371@gmail.com>
2026-02-08 23:42:06 -0800

    Updated script

---
+++ the_creator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create the main directory
+mkdir -p students_directory
+
+# Create cohort files inside the directory
+touch students_directory/cohort-1.txt
+touch students_directory/cohort-2.txt
+touch students_directory/cohort-3.txt
+
+echo "students_directory and cohort files created successfully."
+echo "This is the updated script!"
+
